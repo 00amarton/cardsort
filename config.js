8fb8e482982bfaf93ef9bde2441228df81c16488
00amarton <amarton@consorzioarsenal.it>
2026-05-20 11:22:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,4 @@
 // incolla qui l'URL che termina in /exec
 // ============================================================
 
-var GAS_URL = '';
-// Esempio:
-// var GAS_URL ='https://script.google.com/macros/s/AKfycbwphWfDXLR6ILPfnehjh50URVAuRoR2R6rvxCJwByZqkgjwMu6PBzHjZmpaXhWGIROE1g/exec';
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbwphWfDXLR6ILPfnehjh50URVAuRoR2R6rvxCJwByZqkgjwMu6PBzHjZmpaXhWGIROE1g/exec';
